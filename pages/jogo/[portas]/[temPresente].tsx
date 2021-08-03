4cb2380e515bdd3352ef1aa89e9020892a375cdd
JhonatanFrade <jhonatanfrade.pereira@gmail.com>
2021-08-03 14:03:58 -0300

    feat: Implementado validacao dos valores

--- pages/jogo/[portas]/[temPresente].tsx
+++ pages/jogo/[portas]/[temPresente].tsx
@@ -9,12 +9,23 @@ export default function jogo() {
 
   const router = useRouter()
   const [portas, setPortas] = useState([])
+  const [valido, setValido] = useState(false)
 
   useEffect(() => {
     const portas = +router.query.portas
     const temPresente = +router.query.temPresente
     setPortas(criarPortas(portas, temPresente))
   }, [router?.query])
+
+  useEffect(() => {
+    const portas = +router.query.portas
+    const temPresente = +router.query.temPresente
+    
+    const qtdePortasValida = portas >= 3 && portas <= 100
+    const temPresenteValido = temPresente >= 1 && temPresente <= portas
+
+    setValido(qtdePortasValida && temPresenteValido)
+  }, [portas])
 
   function renderizarPortas() {
     return portas.map(porta => {
@@ -29,7 +40,10 @@ export default function jogo() {
   return (
     <div id={styles.jogo}>
       <div className={styles.portas}>
-        {renderizarPortas()}
+        {valido ? 
+          renderizarPortas() :
+          <h2>Valores inválidos</h2>  
+        }
       </div>
       <div className={styles.botoes}>
         <Link href="/">
