9d9b22081caca7e5089208095919a1b4a096dcbc
JhonatanFrade <jhonatanfrade.pereira@gmail.com>
2021-08-02 21:38:57 -0300

    feat: Integrado Typescript

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,15 @@
 import Presente from "../components/Presente";
 import Porta from "../components/Porta";
+import PortaModel from '../model/porta'
+import { useState } from "react";
 
 export default function Home() {
+
+  const [p1, setP1] = useState(new PortaModel(1))
+
   return (
     <div style={{ display: 'flex'}}>
-      <Porta selecionada/>
-      <Porta />
+      <Porta porta={p1}/>
     </div>
   )
 }
